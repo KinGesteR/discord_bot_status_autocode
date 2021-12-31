c1da9b69ee97ba53caf12bf4fafdd90a669809a4
KinGesteR <96717730+KinGesteR@users.noreply.github.com>
2021-12-31 17:07:55 +0330

    Update minutely.js

--- functions/events/Scheduler/minutely.js
+++ functions/events/Scheduler/minutely.js
@@ -3,10 +3,13 @@ let servers = await lib.discord.guilds['@0.1.0'].list({
   limit: 100,
 });
 let result = await lib.discord.users['@0.1.1'].me.status.update({
-  activity_name: `/help | ${servers.length === 100 ? servers.length + '' : servers.length} server`, // required
+  activity_name: `${servers.length === 100 ? servers.length + '' : servers.length} server`, // required
   activity_type: 'GAME', //Types : GAME , LISTENING , WATCHING , COMPETING
   status: 'ONLINE', //Types : ONLINE , DND , IDLE
 });
 
-//Endpoint type : Scheduler _ Once a minute
-//Change status with server count in bot status
+/*
+Endpoint type : Scheduler _ Once a minute
+Change status with server count in bot status
+Count Code : ${servers.length === 100 ? servers.length + '' : servers.length}
+*/
